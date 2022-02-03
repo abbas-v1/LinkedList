dba7ff65343c525a7208121b355e1e85de8f5744
Muhammad Abbas <abbas@bitoasis.net>
2022-02-03 19:09:56 +0400

    reorder statements in the class

--- Main.java
+++ Main.java
@@ -3,18 +3,16 @@ package net.bitoasis.app;
 public class Main {
 
     public static void main(String[] args) {
+
         Node node1 = new Node(1);
-
         Node node2 = new Node(2);
-        node1.setNext(node2);
-
         Node node3 = new Node(3);
-        node2.setNext(node3);
-
         Node node4 = new Node(4);
-        node3.setNext(node4);
-
         Node node5 = new Node(5);
+
+        node1.setNext(node2);
+        node2.setNext(node3);
+        node3.setNext(node4);
         node4.setNext(node5);
 
         printForward(node1);
